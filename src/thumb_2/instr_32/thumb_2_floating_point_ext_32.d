401bf683b751de9a674aecdf02534e25863da855
nicholashanson <nicholashanson27@outlook.com>
2026-02-25 14:46:56 +0100

    Adding string conversion for vmsr_t1

--- src/thumb_2/instr_32/thumb_2_floating_point_ext_32.d
+++ src/thumb_2/instr_32/thumb_2_floating_point_ext_32.d
@@ -1,4 +1,5 @@
 import std.typecons : Tuple;
+import std.format;
 
 import thumb_2_opcodes;
 import thumb_2_instrs;
@@ -16,4 +17,10 @@ execute_vmsr_t1
 (const ref instr_32 instr, ref vm_t vm) {
 	immutable rt = vm.get_reg(instr.rt);
 	vm.set_fpscr(rt);
+}
+
+// VMRS<c> <Rt>, FPSCR
+string convert_vmrs_t1_to_string(const ref instr_32 instr, const condition cond) {
+	return format("vmrs%s %s, FPSCR", get_condition_string(cond),
+									  get_reg_name(instr.rt));
 }
