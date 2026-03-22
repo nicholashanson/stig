e663c9f6377c004934a97068409ba22ef7ab718d
nicholashanson <nicholashanson27@outlook.com>
2026-03-22 20:47:29 +0100

    Added log file

---
+++ src/log.d
@@ -0,0 +1,50 @@
+module log;
+
+import std.stdio;
+
+private static File* load_store_log_ptr = null;
+
+static string __log_dir__ = "./logs/";
+
+File* load_store_log() {
+    if (load_store_log_ptr is null) {
+        load_store_log_ptr = new File(__log_dir__ ~ "load_store_log.txt", "w");
+    }
+    return load_store_log_ptr;
+}
+
+private static File* pc_log_ptr = null;
+
+File* pc_log() {
+    if (pc_log_ptr is null) {
+        pc_log_ptr = new File(__log_dir__ ~ "pc_log.txt", "w");
+    }
+    return pc_log_ptr;
+}
+
+private static File* stack_log_ptr = null;
+
+File* stack_log() {
+    if (stack_log_ptr is null) {
+        stack_log_ptr = new File(__log_dir__ ~ "stack_log.txt", "w");
+    }
+    return stack_log_ptr;
+}
+
+private static File* uart_log_ptr = null;
+
+File* uart_log() {
+    if (uart_log_ptr is null) {
+        uart_log_ptr = new File(__log_dir__ ~ "uart_log.txt", "w");
+    }
+    return uart_log_ptr;
+}
+
+private static File* gpio_log_ptr = null;
+
+File* gpio_log() {
+    if (gpio_log_ptr is null) {
+        gpio_log_ptr = new File(__log_dir__ ~ "gpio_log.txt", "w");
+    }
+    return gpio_log_ptr;
+}
