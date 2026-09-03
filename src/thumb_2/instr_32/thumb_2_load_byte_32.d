8891541e4805c8dc08268505859b014a45ee73ba
nicholashanson <nicholashanson27@outlook.com>
2026-09-03 18:25:16 +0200

    Parse and execute for qasx_t1

--- src/thumb_2/instr_32/thumb_2_load_byte_32.d
+++ src/thumb_2/instr_32/thumb_2_load_byte_32.d
@@ -564,8 +564,26 @@ void execute_qadd16_t1(vm_t)(const ref instr_32 instr, ref vm_t vm) {
 	vm.set_reg(instr.rd, res);
 }
 
+instr_32 parse_qasx_t1(const uint instr) {
+	return instr_32(rm: cast(reg)slice(instr,  0, 4),
+					rd: cast(reg)slice(instr,  8, 4),
+					rn: cast(reg)slice(instr, 16, 4));
+}
 
-void execute_qasx_t1(vm_t)(const ref instr_32 instr, ref vm_t vm) {}
+void 
+execute_qasx_t1
+(vm_t)
+(const ref instr_32 instr, ref vm_t vm) {
+	uint sum  = cast(uint)cast(short)slice(vm.get_reg(instr.rn),  0, 16) 
+			  + cast(uint)cast(short)slice(vm.get_reg(instr.rm), 16, 16);
+	uint diff = cast(uint)cast(short)slice(vm.get_reg(instr.rn), 16, 16) 
+			  - cast(uint)cast(short)slice(vm.get_reg(instr.rm), 0, 16);
+	ushort sum_sat  = cast(ushort)(sum  > 32767 ? 32767 : (sum  < -32768 ? -32768 : sum));
+    ushort diff_sat = cast(ushort)(diff > 32767 ? 32767 : (diff < -32768 ? -32768 : diff));
+	uint res = (cast(uint)sum_sat << 16) | cast(uint)diff_sat;
+	vm.set_reg(instr.rd, res);
+}
+
 void execute_qsax_t1(vm_t)(const ref instr_32 instr, ref vm_t vm) {}
 
 instr_32 parse_qsub16_t1(const uint instr) {
