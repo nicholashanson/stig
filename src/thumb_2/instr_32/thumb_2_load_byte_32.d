8086dad7706f44b6f9b105836e3ce32829c21d67
nicholashanson <nicholashanson27@outlook.com>
2026-09-04 14:22:16 +0200

    Parse and execute for shsax_t1

--- src/thumb_2/instr_32/thumb_2_load_byte_32.d
+++ src/thumb_2/instr_32/thumb_2_load_byte_32.d
@@ -604,6 +604,24 @@ execute_qsax_t1
 	vm.set_reg(instr.rd, res);
 }
 
+instr_32 parse_shasx_t1(const uint instr) {
+	return instr_32(rm: cast(reg)slice(instr,  0, 4),
+					rd: cast(reg)slice(instr,  8, 4),
+					rn: cast(reg)slice(instr, 16, 4));
+}
+
+void 
+execute_shasx_t1
+(vm_t)
+(const ref instr_32 instr, ref vm_t vm) {
+	uint sum  = cast(uint)cast(short)slice(vm.get_reg(instr.rn),  0, 16) 
+			  + cast(uint)cast(short)slice(vm.get_reg(instr.rm), 16, 16);
+	uint diff = cast(uint)cast(short)slice(vm.get_reg(instr.rn), 16, 16) 
+			  - cast(uint)cast(short)slice(vm.get_reg(instr.rm), 0, 16);
+	uint res = (cast(uint)diff << 16) | cast(uint)sum;
+	vm.set_reg(instr.rd, res);
+}
+
 instr_32 parse_qsub16_t1(const uint instr) {
 	return instr_32(rm: cast(reg)slice(instr,  0, 4),
 					rd: cast(reg)slice(instr,  8, 4),
@@ -641,7 +659,7 @@ execute_shadd16_t1
 	vm.set_reg(instr.rd, res);  
 }
 
-void execute_shasx_t1(vm_t)(const ref instr_32 instr, ref vm_t vm) {}
+
 void execute_shsax_t1(vm_t)(const ref instr_32 instr, ref vm_t vm) {}
 
 instr_32 parse_shsub16_t1(const uint instr) {
