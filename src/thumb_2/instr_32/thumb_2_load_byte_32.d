6f0e4a2378d097c2939ac83724b0301f667fed70
nicholashanson <nicholashanson27@outlook.com>
2026-09-04 17:16:17 +0200

    String conversion for qsub16_t1

--- src/thumb_2/instr_32/thumb_2_load_byte_32.d
+++ src/thumb_2/instr_32/thumb_2_load_byte_32.d
@@ -684,6 +684,13 @@ void execute_qsub16_t1(vm_t)(const ref instr_32 instr, ref vm_t vm) {
 	vm.set_reg(instr.rd, res);
 }
 
+string convert_qsub16_t1_to_string(const ref instr_32 instr, const condition cond) {
+	return format("qsub16%s %s%s, %s", get_condition_string(cond),
+									   (instr.rn == instr.rd) ? "" : (get_reg_name(instr.rd) ~ ", "),
+									   get_reg_name(instr.rn),
+									   get_reg_name(instr.rm));
+}
+
 void execute_qadd8_t1(vm_t)(const ref instr_32 instr, ref vm_t vm) {}
 void execute_qsub8_t1(vm_t)(const ref instr_32 instr, ref vm_t vm) {}
 
